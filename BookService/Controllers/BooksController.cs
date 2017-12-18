d763981fbfeb268288eb3680eb1c1b6fac2b0a56
Katrina R <katrinarosales@live.com>
2017-12-18 02:16:16 +0100

    Details Add

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -18,22 +18,37 @@ namespace BookService.Controllers
         private BookServiceContext db = new BookServiceContext();
 
         // GET: api/Books
-        public IQueryable<Book> GetBooks()
+        public IQueryable<BookDTO> GetBooks()
         {
-            // Eager Loading with Include method
-            // by using System.Data.Entity.Include
-            return db.Books.Include(b => b.Author);
+            var books = from b in db.Books
+                        select new BookDTO()
+                        {
+                            Id = b.Id,
+                            Title = b.Title,
+                            AuthorName = b.Author.Name
+                        };
+
+            return books;
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
+        [ResponseType(typeof(BookDetailDTO))]
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
-            if (book == null)
+            var book = await db.Books.Include(b => b.Author).Select(b =>
+            new BookDetailDTO()
             {
-                return NotFound();
-            }
+                Id = b.Id,
+                Title = b.Title,
+                Year = b.Year,
+                Price = b.Price,
+                AuthorName = b.Author.Name,
+                Genre = b.Genre
+            }).SingleOrDefaultAsync(b => b.Id == id);
+                if (book == null)
+                {
+                    return NotFound();
+                }
 
             return Ok(book);
         }
@@ -85,7 +100,16 @@ namespace BookService.Controllers
             db.Books.Add(book);
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+            db.Entry(book).Reference(x => x.Author).Load();
+
+            var dto = new BookDTO()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name
+            };
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, dto);
         }
 
         // DELETE: api/Books/5
--- BookService/Migrations/Configuration.cs
+++ BookService/Migrations/Configuration.cs
@@ -1,10 +1,10 @@
 namespace BookService.Migrations
 {
+    using BookService.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-    using BookService.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BookService.Models.BookServiceContext>
     {
@@ -16,75 +16,48 @@ namespace BookService.Migrations
         protected override void Seed(BookService.Models.BookServiceContext context)
         {
             context.Authors.AddOrUpdate(x => x.Id,
-                new Author() { Id = 1, Name = "J. K. Rowling" },
-                new Author() { Id = 2, Name = "Stephen King" },
-                new Author() { Id = 3, Name = "Jeff Kinney" },
-                new Author() { Id = 4, Name = "Dr. Seuss" }
+                new Author() { Id = 1, Name = "Jane Austen" },
+                new Author() { Id = 2, Name = "Charles Dickens" },
+                new Author() { Id = 3, Name = "Miguel de Cervantes" }
                 );
 
-            context.Books.AddOrUpdate(
-                new Book () {
+            context.Books.AddOrUpdate(x => x.Id,
+                new Book()
+                {
                     Id = 1,
-                    Title = "Harry Potter and the Prisoner of Azkaban",
-                    Year = 2017,
+                    Title = "Pride and Prejudice",
+                    Year = 1813,
                     AuthorId = 1,
                     Price = 9.99M,
-                    Genre = "Children's Books"
+                    Genre = "Comedy of manners"
                 },
                 new Book()
                 {
                     Id = 2,
-                    Title = "Fantastic Beasts and Where to Find Them",
-                    Year = 2017,
+                    Title = "Northanger Abbey",
+                    Year = 1817,
                     AuthorId = 1,
-                    Price = 8.30M,
-                    Genre = "Children's Books"
-                }
-                , new Book()
+                    Price = 12.95M,
+                    Genre = "Gothic parody"
+                },
+                new Book()
                 {
                     Id = 3,
-                    Title = "IT",
-                    Year = 2017,
+                    Title = "David Copperfield",
+                    Year = 1850,
                     AuthorId = 2,
-                    Price = 10.41M,
-                    Genre = "Thriller"
+                    Price = 15,
+                    Genre = "Bildungsroman"
                 },
                 new Book()
                 {
                     Id = 4,
-                    Title = "The Dark Tower",
-                    Year = 2016,
-                    AuthorId = 2,
-                    Price = 7.58M,
-                    Genre = "Fiction"
-                },
-                new Book()
-                {
-                    Id = 5,
-                    Title = "The Getaway",
-                    Year = 2017,
+                    Title = "Don Quixote",
+                    Year = 1617,
                     AuthorId = 3,
-                    Price = 9.99M,
-                    Genre = "Children's Books"
-                },
-                new Book()
-                {
-                    Id = 6,
-                    Title = "You're Only Old Once!: A Book for Obsolete Children",
-                    Year = 2009,
-                    AuthorId = 4,
-                    Price = 8.91M,
-                    Genre = "Humor"
-                },
-                new Book()
-                {
-                    Id = 7,
-                    Title = "How the Grinch Stole Christmas!",
-                    Year = 1957,
-                    AuthorId = 4,
-                    Price = 9.44M,
-                    Genre = "Children's Books"
-                } 
+                    Price = 8.95M,
+                    Genre = "Picaresque"
+                }
                 );
         }
     }
